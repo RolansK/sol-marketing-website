a7d914db54e4ce54e10b4b042f0821a8225b1e9b
RolansK <rolanskims@gmail.com>
2025-03-10 18:44:46 +0100

    added hero images with all the styling

--- src/lib/sections/Hero.svelte
+++ src/lib/sections/Hero.svelte
@@ -1,18 +1,22 @@
 <script>
-	import ButtonCta from '$lib/components/ButtonCta.svelte';
-
+	import ButtonPrimary from '$lib/components/ButtonPrimary.svelte';
 	import Eye from '$lib/icons/eye.svelte';
 </script>
 
 <section class="hero">
 	<div>
+		<img class="hero-image" src="/src/lib/pictures/hero005.jpg" alt="Hero 5" />
+		<img class="hero-image" src="/src/lib/pictures/hero004.jpg" alt="Hero 4" />
+		<img class="hero-image" src="/src/lib/pictures/hero003.jpg" alt="Hero 3" />
+		<img class="hero-image" src="/src/lib/pictures/hero002.jpg" alt="Hero 2" />
+		<img class="hero-image" src="/src/lib/pictures/hero001.jpg" alt="Hero 1" />
 		<span>Loved by pros</span>
 		<span>Visual</span>
 		<Eye />
 		<span>plugin</span>
 		<span>for Framer</span>
 
-		<ButtonCta icon={Eye} title="Watch the demo" />
-		<ButtonCta icon={Eye} title="Follow on X" />
+		<ButtonPrimary title="Watch the demo" />
+		<ButtonPrimary title="Follow on X" />
 	</div>
 </section>
