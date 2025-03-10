d748c06d161eab2868501a96e26c8acd0f48765e
RolansK <rolanskims@gmail.com>
2025-03-10 14:27:01 +0100

    happy with the sidebar for now

---
+++ src/lib/components/ButtonCta.svelte
@@ -0,0 +1,11 @@
+<script>
+	const { icon, title = 'Demo' } = $props();
+</script>
+
+<button class="button-cta bottom">
+	{#if icon}
+		{@const Icon = icon}
+		<Icon class="icon" />
+	{/if}
+	<span>{title}</span>
+</button>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,10 +1,13 @@
 <script>
 	import { page } from '$app/stores';
 	import ButtonNav from '$lib/components/ButtonNav.svelte';
+	import ButtonCta from '$lib/components/ButtonCta.svelte';
+
 	import Play from '$lib/assets/play.svelte';
 	import PuzzlePiece from '$lib/assets/puzzle-piece.svelte';
 	import Star from '$lib/assets/star.svelte';
 	import SealQuestion from '$lib/assets/seal-question.svelte';
+	import Sparkle from '$lib/assets/sparkle.svelte';
 </script>
 
 <nav>
@@ -12,4 +15,5 @@
 	<ButtonNav icon={PuzzlePiece} title="Components" />
 	<ButtonNav icon={Star} title="Features" />
 	<ButtonNav icon={SealQuestion} title="FAQ" />
+	<ButtonCta icon={Sparkle} title="Get Sol" />
 </nav>
