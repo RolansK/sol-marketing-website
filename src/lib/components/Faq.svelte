4361c1c596147c4e900dbb3299a6e26f13f4291f
RolansK <rolanskims@gmail.com>
2025-03-20 14:06:50 +0100

    added faq, I think

--- src/lib/components/Faq.svelte
+++ src/lib/components/Faq.svelte
@@ -1,25 +1,35 @@
 <script>
+	import Plus from '$lib/icons/plus.svelte';
 	import { slide } from 'svelte/transition';
 
 	const faqs = [
 		{
-			question: 'Can I use Sol in commercial projects?',
-			answer:
-				'Yes, you can use Sol in unlimited personal and client projects. For template usage, please contact us.'
+			question: 'How do I request a feature?',
+			answer: 'Use the contact form in the plugin settings or simply email us.'
 		},
 		{
-			question: 'Do you offer refunds?',
+			question: 'How do I report a bug?',
 			answer:
-				'Due to the digital nature of the product, we cannot offer refunds. Please reach out if you have any issues with your purchase.'
+				'Same as requesting a feature - use the contact form in the plugin settings or simply email us.'
 		},
 		{
-			question: 'How do I get support?',
-			answer:
-				'We offer priority email support for all customers. Just drop us a line at support@sol.com.'
+			question: 'How fast do you reply to emails?',
+			answer: 'Typically, within 48 hours or less.'
 		},
 		{
-			question: 'Will there be future updates?',
-			answer: 'Yes, all future updates are included in your lifetime license.'
+			question: 'Can I use Sol in my Framer template?',
+			answer:
+				'If you are planning to use it in templates, please email us. Use in templates without our permission is not allowed.'
+		},
+		{
+			question: 'Are animations gifs or videos?',
+			answer:
+				'Neither, actually! Everything is a code snippet that browser renders real-time. In practice, it means super tiny size and fast performance.'
+		},
+		{
+			question: 'Can I get notified about updates?',
+			answer:
+				'Yes! We will let you know via email once a new update is out. You can also check our X/Twitter account.'
 		}
 	];
 
@@ -32,21 +42,17 @@
 
 <div class="faq-container">
 	<h1>FAQ</h1>
-	<div class="faq-items">
-		{#each faqs as faq, i}
-			<div class="faq-item">
-				<button class="faq-question" class:active={activeIndex === i} on:click={() => toggleFaq(i)}>
-					{faq.question}
-					<svg width="20" height="20" viewBox="0 0 20 20" class:rotated={activeIndex === i}>
-						<path d="M7 10L10 13L13 10" stroke="currentColor" fill="none" stroke-width="1.5" />
-					</svg>
-				</button>
-				{#if activeIndex === i}
-					<div class="faq-answer">
-						<p>{faq.answer}</p>
-					</div>
-				{/if}
-			</div>
-		{/each}
-	</div>
+	{#each faqs as faq, i}
+		<div class="faq-item">
+			<button class="faq-question" class:active={activeIndex === i} on:click={() => toggleFaq(i)}>
+				<Plus />
+				{faq.question}
+			</button>
+			{#if activeIndex === i}
+				<div class="faq-answer">
+					<p>{faq.answer}</p>
+				</div>
+			{/if}
+		</div>
+	{/each}
 </div>
