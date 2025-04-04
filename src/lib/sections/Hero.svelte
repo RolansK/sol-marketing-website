34e83b13c1d74d9a7c117bfc4c7f58b669bf7e2e
RolansK <rolanskims@gmail.com>
2025-04-04 18:39:54 +0200

    changed some image-related path issue

--- src/lib/sections/Hero.svelte
+++ src/lib/sections/Hero.svelte
@@ -1,15 +1,20 @@
 <script>
 	import Button from '$lib/components/Button.svelte';
 	import Eye from '$lib/icons/eye.svelte';
+	import hero001 from '$lib/pictures/hero001.jpg';
+	import hero002 from '$lib/pictures/hero002.jpg';
+	import hero003 from '$lib/pictures/hero003.jpg';
+	import hero004 from '$lib/pictures/hero004.jpg';
+	import hero005 from '$lib/pictures/hero005.jpg';
 </script>
 
 <section class="hero">
 	<div>
-		<img src="/src/lib/pictures/hero005.jpg" alt="Hero 5" />
-		<img src="/src/lib/pictures/hero004.jpg" alt="Hero 4" />
-		<img src="/src/lib/pictures/hero003.jpg" alt="Hero 3" />
-		<img src="/src/lib/pictures/hero002.jpg" alt="Hero 2" />
-		<img src="/src/lib/pictures/hero001.jpg" alt="Hero 1" />
+		<img src={hero005} alt="Hero 5" />
+		<img src={hero004} alt="Hero 4" />
+		<img src={hero003} alt="Hero 3" />
+		<img src={hero002} alt="Hero 2" />
+		<img src={hero001} alt="Hero 1" />
 		<span>Loved by pros</span>
 		<h1>Visual</h1>
 		<Eye />
