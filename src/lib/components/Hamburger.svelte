bd07c16ae6644c99ee4000a9aeb6d2ca1a753e29
RolansK <rolanskims@gmail.com>
2025-04-04 15:28:04 +0200

    improved hamburger agaij

--- src/lib/components/Hamburger.svelte
+++ src/lib/components/Hamburger.svelte
@@ -1,16 +1,6 @@
 <script>
 	let isOpen = $state(false);
-
-	let {
-		hamburgerClick,
-		unit = 'px',
-		width = 32,
-		height = 24,
-		lineHeight = 2,
-		contain = false
-	} = $props();
-
-	let offset = $derived(height / 2 - lineHeight / 2);
+	let { hamburgerClick } = $props();
 
 	function toggleMenu() {
 		isOpen = !isOpen;
@@ -18,19 +8,7 @@
 	}
 </script>
 
-<!-- svelte-ignore a11y_click_events_have_key_events -->
-<!-- svelte-ignore a11y_no_static_element_interactions -->
-<div onclick={toggleMenu} class="hamburger" class:isOpen>
-	<div
-		style:transform={isOpen ? `translate(0, ${offset}${unit}) rotate(45deg)` : ''}
-		style:width={isOpen && contain
-			? Math.sqrt(2 * Math.pow(width > height ? height : width, 2)) - lineHeight + unit
-			: width + unit}
-	></div>
-	<div
-		style:transform={isOpen ? `translate(0, -${offset}${unit}) rotate(-45deg)` : ''}
-		style:width={isOpen && contain
-			? Math.sqrt(2 * Math.pow(width > height ? height : width, 2)) - lineHeight + unit
-			: width + unit}
-	></div>
-</div>
+<button onclick={toggleMenu} class="hamburger" class:open={isOpen}>
+	<div class:open={isOpen}></div>
+	<div class:open={isOpen}></div>
+</button>
